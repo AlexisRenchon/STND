b73fcbbdce434f09795927289d35c251b206ce2f
AlexisRenchon <a.renchon@gmail.com>
2021-04-28 17:06:13 -0500

    up

--- Interac_viz_live.jl
+++ Interac_viz_live.jl
@@ -103,14 +103,14 @@ for i = 1:length(Data_Col_2016)
 	end
 end
 
-DATA_Col_2018 = []
-datetime_Col_2018 = []
-for i = 1:length(Data_Col_2018)
+DATA_Col_2017 = []
+datetime_Col_2017 = []
+for i = 1:length(Data_Col_2017)
 	try
-	push!(DATA_Col_2018, CSV.read(Data_Col_2018[i], DataFrame; dateformat = "HH:MM:SS")) 
+	push!(DATA_Col_2017, CSV.read(Data_Col_2017[i], DataFrame; dateformat = "HH:MM:SS")) 
 	#types = [Int64, Int64, Int64, Time, Float64, Float64, Float64, Float64, Float64, Float64])) #for i = 1:length(Data_Col_2016)];
-	DATA_Col_2018[i] = dropmissing(DATA_Col_2018[i]); # get rid of broken rows
-	push!(datetime_Col_2018, Date.(DATA_Col_2018[i].Year.+2000, DATA_Col_2018[i].Month, DATA_Col_2018[i].Day) .+ DATA_Col_2018[i].Time)
+	DATA_Col_2017[i] = dropmissing(DATA_Col_2017[i]); # get rid of broken rows
+	push!(datetime_Col_2017, Date.(DATA_Col_2017[i].Year.+2000, DATA_Col_2017[i].Month, DATA_Col_2017[i].Day) .+ DATA_Col_2017[i].Time)
 	catch
 		nothing
 	end
