3d05bd8217f59561f5142066c1e65dc020fcb910
AlexisRenchon <a.renchon@gmail.com>
2021-05-04 11:43:58 -0500

    update

--- Load_DataFrames.jl
+++ Load_DataFrames.jl
@@ -10,19 +10,33 @@ Sites, n_S, Years, n_Y, rsoilp = rsoilpath(); # example rsoilp["Col"]["2017"][1]
 Data = Dict(Sites .=> [Dict(Years .=> [[] for i in 1:n_Y]) for i in 1:n_S])
 datetime = Dict(Sites .=> [Dict(Years .=> [[] for i in 1:n_Y]) for i in 1:n_S])
 
-readata(x) = (y = CSV.read(x, DataFrame, dateformat = "HH:MM:SS", types=Dict("Time" => Time), silencewarnings=true); df = dropmissing(y)) 
+Types = Dict(["Month", "Day", "Year", "Time", "Flux", "Temperature (C)"] .=> [Int64, Int64, Int64, Time, Float64, Float64])
+readata(x) = (y = DataFrame(CSV.File(x, dateformat = "HH:MM:SS", types = Types, silencewarnings=true)); df = dropmissing(y); df = delete!(df, findall(df.Month .> 12)); df = delete!(df, findall(df.Month .< 1)); df = delete!(df, findall(df.Year .> 21)); df = delete!(df, findall(df.Day .< 1)); df = delete!(df, findall(df.Day .> 31))) 
 readate(x) = Date.(x.Year.+2000, x.Month, x.Day) .+ x.Time
 
-# Line below needs to be fixed. 
-# e.g. rsoilp["Col"]["2017"] has 8 folders, each folder has ~ 8 .csv files
 # one option would be Data["Col"]["2017"]["folder_i"]["file_j"]
 # I could create 2 menu which give name of folder (menu 1) and name of files (menu 2)
 # Other, better option is by plot (e.g. Data["Col"]["2017"]["A"] ) 
+
+# troubleshooting:
+# files IL-2D_FD_20160012_2020-05-15_12-57-32.csv 
+# and IL-4A_FD_20160030_2020-05-15_12-29-35.csv needs to be deleted, not a data file
+# in Fermilab STND Files/Fermilab 2020 Files/FRMI 2020 Soil Respiration/5-15-20
 [[[[push!(Data[S][Y], readata(rsoilp[S][Y][j][i])) for i = 1:length(rsoilp[S][Y][j])] for j = 1:length(rsoilp[S][Y])] for S in Sites] for Y in Years];
 
+# the code below is same as the line above, but can be useful for troubleshooting
+# for Y in Years
+#	for S in Sites
+#		for j in 1:length(rsoilp[S][Y])
+#			for i in 1:length(rsoilp[S][Y][j])
+#				push!(Data[S][Y], readata(rsoilp[S][Y][j][i]));
+#				println("Year ", Y, " Site ", S, " j ", j, " i ", i)
+#			end
+#		end
+#	end
+# end
 
-# need to troubleshoot line below, at what S Y i does it bug?
-[[[push!(datetime[S][Y], readate(Data[S][Y][i])) for i = 1:length(Data[S][Y])] for S in Sites] for Y in Years]
+[[[push!(datetime[S][Y], readate(Data[S][Y][i])) for i = 1:length(Data[S][Y])] for S in Sites] for Y in Years];
 
 return Data, datetime, Sites, n_S, Years, n_Y
 
